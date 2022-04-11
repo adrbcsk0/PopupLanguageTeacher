b7e70b9ea270988f09e0bc536e97051eeaa1d5f6
adrbcsk0 <78171744+adrbcsk0@users.noreply.github.com>
2022-04-11 18:55:01 +0200

    Time sleep added between toast appearance.

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 from win10toast import ToastNotifier
 import random
+import time
 from dictionaries import dictDE
 from dictionaries import dictPL
 
-x = random.randint(1, len(dictDE))
-x = str(x)
+# on this stage I coded as many popups as words we have in dictionary. In next versions I would like to let user to
+# decided how many popups and how often will appear.
 
-# print(dictDE.get(x))
+for i in range (1, 5):                          #hardcoded for testing purpose
+    y = random.randint(1, 2)
+    x = random.randint(1, len(dictDE))
+    t = random.randint(60, 120)                 #hardcoded for testing purpose
+    x = str(x)
 
-y = random.randint(1, 2)
-
-if y == 1:
-    toaster = ToastNotifier()
-    toaster.show_toast("Zgadnij słówko", dictDE.get(x), duration=10)
-    toaster2 = ToastNotifier()
-    toaster2.show_toast("Zgadnij słówko", dictPL.get(x), duration=10)
-else:
-    toaster2 = ToastNotifier()
-    toaster2.show_toast("Zgadnij słówko", dictPL.get(x), duration=10)
-    toaster = ToastNotifier()
-    toaster.show_toast("Zgadnij słówko", dictDE.get(x), duration=10)
-
-
+    if y == 1:
+        toaster = ToastNotifier()
+        toaster.show_toast("Zgadnij słówko", dictDE.get(x), duration=6)
+        toaster2 = ToastNotifier()
+        toaster2.show_toast("Zgadnij słówko", dictPL.get(x), duration=6)
+    else:
+        toaster2 = ToastNotifier()
+        toaster2.show_toast("Zgadnij słówko", dictPL.get(x), duration=6)
+        toaster = ToastNotifier()
+        toaster.show_toast("Zgadnij słówko", dictDE.get(x), duration=6)
+    time.sleep(t)
+    i += 1
 
 # toaster.show_toast("Example two",
 #                    "This notification is in it's own thread!",
